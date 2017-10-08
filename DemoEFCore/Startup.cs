f6949595959acc8066523ab796cfba32ddd44bbc
Poy Chang <poypost@gmail.com>
2017-10-08 11:01:20 +0800

    1. 在 ConfigureServices 注入 MyDbContext，並設定其資料庫連線字串 2. 在 Configure 啟動階段中，於最後時呼叫 dbContext.Database.EnsureCreated() 建立資料庫

--- DemoEFCore/Startup.cs
+++ DemoEFCore/Startup.cs
@@ -4,10 +4,12 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using DemoEFCore.DataAccessLayer;
 
 namespace DemoEFCore
 {
@@ -23,13 +25,16 @@ namespace DemoEFCore
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MyDbContext>(options =>
+                options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory, MyDbContext dbContext)
         {
             app.UseMvc();
+            dbContext.Database.EnsureCreated();
         }
     }
 }
