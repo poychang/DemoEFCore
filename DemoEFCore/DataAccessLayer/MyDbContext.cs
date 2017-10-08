decd398e831c2716c79dbd729c86025c2dc24e85
Poy Chang <poypost@gmail.com>
2017-10-08 11:01:04 +0800

    建立 DbContext 及 DbSet，並建立對應 DbSet 的資料模型

---
+++ DemoEFCore/DataAccessLayer/MyDbContext.cs
@@ -0,0 +1,12 @@
+﻿using DemoEFCore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoEFCore.DataAccessLayer
+{
+    public class MyDbContext : DbContext
+    {
+        public MyDbContext(DbContextOptions<MyDbContext> options) : base(options) { }
+
+        public DbSet<Employee> Employee { get; set; }
+    }
+}
---
+++ DemoEFCore/Models/Employee.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DemoEFCore.Models
+{
+    public class Employee
+    {
+        [Key, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+    }
+}
